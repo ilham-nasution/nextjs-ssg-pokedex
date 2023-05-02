3a3994078dd999e4e9cca2bfa70c277bfc0efa40
Ilham Rahman <ilhaamrahman@gmail.com>
2023-05-02 08:12:10 +0700

    fix type

--- components/PokemonType.tsx
+++ components/PokemonType.tsx
@@ -1,4 +1,8 @@
-export default function PokemonType({ type }) {
+export default function PokemonType({
+  type,
+}: {
+  type: { type: { name: string } };
+}) {
   return (
     <p className="rounded-lg shadow-lg text-center text-white p-1 w-24 bg-white/10 text-xs m-1">
       {type.type.name}
